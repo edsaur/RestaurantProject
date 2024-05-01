ba0713d587d7702302b11fe091acd32bbacb05ce
Edzer Dionido <jeddionido26@gmail.com>
2024-05-01 17:02:55 +0800

    Add event listener to the review button

--- customerSide/js/app.js
+++ customerSide/js/app.js
@@ -2,6 +2,8 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
+const review_btn = document.querySelector('#review-btn');
+
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
@@ -22,4 +24,8 @@ menu_item.forEach((item) => {
 		hamburger.classList.toggle('active');
 		mobile_menu.classList.toggle('active');
 	});
+});
+
+review_btn.addEventListener('click', () => {
+	location.href = "/customerSide/reviews/write-reviews.php";
 });

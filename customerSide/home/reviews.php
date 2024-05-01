ad87587f585cffdf3b651820698dd3b5f85536fb
Edzer Dionido <jeddionido26@gmail.com>
2024-05-01 17:00:30 +0800

    add review webpage and its contents

--- customerSide/home/reviews.php
+++ customerSide/home/reviews.php
@@ -1,4 +1,6 @@
-<?php include_once('../components/header.php'); ?>
+<?php 
+include_once('../components/header.php'); 
+?>
 <section id="hero" style="position: relative;">
     <video autoplay loop muted playsinline poster="your-poster-image.jpg"
         style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover;">
@@ -24,15 +26,39 @@
 
 
 <section id="reviews">
-    <div class="container">
+    <div class="reviews container">
         <h1 class="section-title">See What People <span>Says!</span></h1>
-
-        <div class="reviews-box">
-            <div class="reviews">
-                
+        <div id="reviews-box">
+            <div class="review">
+                <div>
+                    <h2>Great Restaurant!</h2>
+                    <ul class="star-rating">
+                        <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                        <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                        <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                        <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                        <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+                    </ul>
+                </div>
+                <h3>From: Gordon Ramsey</h3>
+                <p>Food was great but the place looks awesome! One thing I didn't liked is the child crying.</p>
             </div>
-        </div>
+            <div class="review">
+                <h2>Title</h2>
+                <h3>From: Gordon Ramsey</h3>
+                <p>Content bla bla blah</p>
+            </div>
+            <div class="review">
+                <h2>Title</h2>
+                <h3>From: Gordon Ramsey</h3>
+                <p>Content bla bla blah</p>
+            </div>
     </div>
+        <button id="review-btn">WRITE YOUR REVIEW HERE!</button>
+
+    </div>
+
+
 </section>
 
 <?php include_once('../components/footer.php'); ?>
